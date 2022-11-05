22baa43c01bc489826bf5b9655a38165350d6e5e
Bert <bertrandmuscat@gmail.com>
2022-11-05 18:12:54 +0100

    correction css responsive  + issue 1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,5 @@
+// responsive nav
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,17 +9,50 @@ function editNav() {
   }
 }
 
+// launch modal
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// blur background
+const mainExceptModal = document.querySelector("main *:not(.content)");
+const header = document.querySelector(".topnav");
+const footer = document.querySelector("footer");
+
+function blurBg(pxAmount) {
+  mainExceptModal.style.filter = `blur(${pxAmount})`;
+  header.style.filter = `blur(${pxAmount})`;
+  footer.style.filter = `blur(${pxAmount})`;
+  if (window.innerWidth < 540) {
+    header.style.filter = `blur(0px)`;
+  }  
+} 
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  blurBg('10px');
 }
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+
+
+// issue #1 : X close modal
+
+const closeX = document.querySelector(".close");
+
+function closeModal() {
+  modalbg.style.display = "none";
+  blurBg('0px');
+}
+
+closeX.addEventListener("click", closeModal);
+
+
+//issue #2 : form validation
 
 
